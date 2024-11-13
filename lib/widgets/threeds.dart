d42003b19a008f3fc1f127faa87893e0acdf80b1
yoseph <yosephhabtamu5@gmail.com>
2024-11-13 19:06:59 +0300

    platform dependent web render

--- lib/widgets/threeds.dart
+++ lib/widgets/threeds.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'dart:io' show Platform;
+import 'dart:html' as html; // Only used on the web
+import 'dart:ui' as ui; // Only used on the web
 
 class ThreeDSPage extends StatefulWidget {
   final String? html;
@@ -20,60 +22,62 @@ class _ThreeDSPageState extends State<ThreeDSPage> {
   void initState() {
     super.initState();
 
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..addJavaScriptChannel('Toaster', onMessageReceived: (message) {
-        // ignore: deprecated_member_use
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(message.message)),
-        );
-      })
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            // Update loading bar.
-          },
-          onPageStarted: (String url) {
-            debugPrint('Page started loading: $url, ${widget.returnURL}');
-            if (url.startsWith(widget.returnURL ?? "")) {
-              Navigator.of(context).pop();
-            }
-          },
-          onPageFinished: (String url) {},
-          onWebResourceError: (WebResourceError error) {
-            debugPrint('WebResourceError: $error');
-
-            if (Platform.isIOS) {
-              Navigator.of(context).pop();
-            }
-          },
-          onNavigationRequest: (NavigationRequest request) {
-            debugPrint('allowing navigation to $request');
-            if (request.url.startsWith(widget.returnURL ?? "")) {
-              Navigator.of(context).pop();
-              return NavigationDecision.prevent;
-            }
-            return NavigationDecision.navigate;
-          },
-        ),
-      )
-      ..loadHtmlString(widget.html.toString());
+    if (kIsWeb) {
+      // Register the view factory for web only
+      //ignore: undefined_prefixed_name
+      ui.platformViewRegistry.registerViewFactory(
+        'iframe-html-viewer',
+        (int viewId) {
+          final iframe = html.IFrameElement()
+            ..srcdoc = widget.html ?? ""
+            ..style.border = 'none';
+          return iframe;
+        },
+      );
+    } else {
+      // Initialize WebViewController for mobile platforms only
+      _controller = WebViewController()
+        ..setJavaScriptMode(JavaScriptMode.unrestricted)
+        ..setBackgroundColor(const Color(0x00000000))
+        ..addJavaScriptChannel('Toaster', onMessageReceived: (message) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        })
+        ..setNavigationDelegate(
+          NavigationDelegate(
+            onProgress: (int progress) {},
+            onPageStarted: (String url) {
+              if (url.startsWith(widget.returnURL ?? "")) {
+                Navigator.of(context).pop();
+              }
+            },
+            onPageFinished: (String url) {},
+            onWebResourceError: (WebResourceError error) {
+              if (Platform.isIOS) {
+                Navigator.of(context).pop();
+              }
+            },
+            onNavigationRequest: (NavigationRequest request) {
+              if (request.url.startsWith(widget.returnURL ?? "")) {
+                Navigator.of(context).pop();
+                return NavigationDecision.prevent;
+              }
+              return NavigationDecision.navigate;
+            },
+          ),
+        )
+        ..loadHtmlString(widget.html.toString());
+    }
   }
-
-  String url = "";
-  double progress = 0;
-  final urlController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("3DS")),
-      body: Builder(builder: (BuildContext context) {
-        return WebViewWidget(
-          controller: _controller,
-        );
-      }),
+      appBar: AppBar(title: Text("3DS")),
+      body: kIsWeb
+          ? HtmlElementView(viewType: 'iframe-html-viewer') // For web
+          : WebViewWidget(controller: _controller), // For mobile
     );
   }
 }
